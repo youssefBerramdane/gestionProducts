e8af166bcd4bd5bffb57554ca01b9c0ca6c185b3
Youssef Berramdane <yberramdane@smb.sqli.com>
2024-03-19 13:12:55 +0000

    initialProject

---
+++ src/main/java/com/sqli/gestionproject/GestionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sqli.gestionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sqli/gestionproject/GestionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqli.gestionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
